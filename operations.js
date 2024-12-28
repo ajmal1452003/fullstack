f55b0730fb8aad3aae22fe5391ba02f3c140c30f
Ajmal <ajmal1452003@gmail.com>
2024-12-28 11:24:14 +0530

    Adding js

---
+++ operations.js
@@ -0,0 +1,15 @@
+var x = 10;
+var y = 20;
+console.log("Sum:",x + y);
+
+console.log("Dif:",x - 5);
+
+var z = 10;
+if(x==z){
+    console.log(" same");
+}
+if(x==="10"){
+    console.log("same");
+}else{
+    console.log("not same.");
+}
