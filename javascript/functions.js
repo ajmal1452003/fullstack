5c8ae55488f57dceb9a94f660930eba8389ea9fb
Ajmal <ajmal1452003@gmail.com>
2024-12-28 16:23:28 +0530

    Adding Javascript

--- javascript/functions.js
+++ javascript/functions.js
@@ -18,3 +18,42 @@ arrowfunc2 = (stud) => {
 }
 arrowfunc2(["CSE","IT","AIML"]);
 //eg3
+//destruction operator
+var marks =[89,73,84]
+var [m1,m2,m3] = marks
+console.log(m1,m2,m3)
+//ternary operator
+
+a=10
+out = (a%2 == 0) ? "even": "odd"
+console.log(a,"is",out)
+
+//Spread operator
+
+arr1 = [10,20,30]
+arr2 = [40,50,60]
+arr3 = [...arr1,...arr2]
+console.log(arr3)
+
+//Rest operator
+function restOPEx(...args){
+    console.log(args)
+}
+restOPEx("CSE","IT","AIML",10,20,30)
+
+//Scope
+var a = 5;
+const p = 3.14
+console.log("o",a);
+console.log("o",p);
+{
+    let a = 54;
+    let b=64;
+    var c=31;
+    console.log("b",a);
+    console.log("b",b);
+    console.log("b",c);
+}
+    console.log("o",a);
+    console.log("o",p);
+    console.log("o",c);
---
+++ javascript/hoisting.js
@@ -0,0 +1,14 @@
+console.log(a);//OUTPUT: undefined
+var a = 20;
+console.log(a);//OUTPUT: 20
+
+console.log(b);
+const b = 3.14;
+console.log(b);
+//OUTPUT: ReferenceError: Cannot access 'b' before initialization
+
+console.log(c);
+let c = 4;
+console.log(c);
+//OUTPUT: ReferenceError: Cannot access 'c' before initialization
+
