c26cbd84edfe7fe56ac9e52de6a22c01b1282b9b
Ajmal <ajmal1452003@gmail.com>
2024-12-28 14:02:36 +0530

    Adding Javascript

---
+++ javascript/functions.js
@@ -0,0 +1,20 @@
+arrowfunc = (stud) => {
+    console.log(stud)
+}
+arrowfunc(["CSE","IT","AIML"]);
+//eg1
+
+
+
+arrowfunc1 = (stud) => {
+    console.log(stud[0])
+}
+arrowfunc1(["CSE","IT","AIML"]);
+//eg2
+
+
+arrowfunc2 = (stud) => {
+    console.log(stud[2])
+}
+arrowfunc2(["CSE","IT","AIML"]);
+//eg3
